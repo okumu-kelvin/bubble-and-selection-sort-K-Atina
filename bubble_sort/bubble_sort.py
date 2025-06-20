90ffeee80f36d623d4ddafa2ecd337bbba0dcbf7
Atina <149969565+K-Atina@users.noreply.github.com>
2025-06-20 14:13:05 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    def bubble_sort(unsorted_list):
+    #To access each element in the list,
+        for i in range(len(unsorted_list)):
+            #To comapare the current element with its next element.
+            for j in range(0, len(unsorted_list) - i -1):
+                #If the current element is greater than the next element...
+                if unsorted_list[j] > unsorted_list[j+1]:
+                    # Swapping the elements
+                    temp = unsorted_list[j]
+                    unsorted_list[j] = unsorted_list[j+1]
+                    unsorted_list[j+1] = temp
+    return unsorted_list
